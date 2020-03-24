c53071a3d8ecc6c96215e2e4210e246994556cd8
PolaZwardon <pola.zwardon@gmail.com>
2020-03-24 18:36:55 +0100

    Updated HomePage About

--- src/HomePage/HomePage.jsx
+++ src/HomePage/HomePage.jsx
@@ -1,8 +1,6 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { Button, Card } from 'react-bootstrap';
-import Form from 'react-jsonschema-form';
 import Carousel from 'react-bootstrap/Carousel'
 import Image from 'react-bootstrap/Image'
 
@@ -11,19 +9,14 @@ import { userActions } from '../_actions';
 import ProfileManagementBar from "../_components/ProfileManagementBar";
 import IconsBar from "../_components/IconsBar";
 import NavbarComponent from "../_components/NavbarComponent";
-import AddEventButton from "../_components/AddEventButton";
-import EventBoxes from "../_components/EventBoxes";
 import Footer from "../_components/Footer";
 import '../css/home.css';
-import ChangeUserNameForm from "../_components/ChangeUserNameForm";
+import '../css/navbar.css';
+import '../css/footer.css';
 
 const community1 = require('../img/join.png').default;
 const community2 = require('../img/multicultural1.png').default;
 const community3 = require('../img/multicultural2.png').default;
-
-
-
-
 
 class HomePage extends React.Component {
 
@@ -31,10 +24,13 @@ class HomePage extends React.Component {
 
         return (
 
-            <div class="home">
+            <div class="page">
                 <ProfileManagementBar/>
                 <IconsBar/>
                 <NavbarComponent/>
+                <Card.Header class="homePage-header">
+                    <h2>Let's get to know each other!</h2>
+                </Card.Header>
                 <div class="home-carousel">
                 <Carousel>
                     <Carousel.Item>
@@ -42,8 +38,6 @@ class HomePage extends React.Component {
                             className="d-block w-100"
                             src={community3}
                             alt="First slide"
-
-
                         />
                         <Carousel.Caption>
                             <h3>Share your passions with others!</h3>
@@ -54,8 +48,6 @@ class HomePage extends React.Component {
                             className="d-block w-100"
                             src={community2}
                             alt="First slide"
-
-
                         />
                         <Carousel.Caption>
                             <h3>Meet new friends and gain experiences!</h3>
@@ -66,15 +58,51 @@ class HomePage extends React.Component {
                             className="d-block w-100"
                             src={community1}
                             alt="First slide"
-
-
                         />
 
                     </Carousel.Item>
 
                 </Carousel>
                 </div>
+                <div>
+                    <Card className="contact-container">
 
+                        <Card.Body class="about-body">
+                            <Card.Header class="homePage-section-header">
+                                <h2>About</h2>
+                            </Card.Header>
+                            <Card.Subtitle class="section-information">
+                                <p>&emsp;Good2Meet is a place where you can find others to spend time with and share experiences. If you and your football team is looking for eleventh player just... </p>
+                            </Card.Subtitle>
+                            <Card.Header class="homePage-section-header">
+                                <h2>Create an event</h2>
+                            </Card.Header>
+                            <Card.Subtitle class="section-information">
+                                <p>&emsp;Go to event page, click "Add Event", write a short description, choose time, place, how many people you are looking for and create your event! Just like that!</p>
+                                <p>&emsp;You can also choose one of categories to help others find your event. Write us if you are unable to find matching category, we are open to changes!</p>
+                                <p>&emsp;After someone join your event, we will let you know via email and send you emails to your all event participants. This way you will be able to discuss more details.</p>
+
+
+                            </Card.Subtitle>
+                            <Card.Header class="homePage-section-header">
+                                <h2>Join the event</h2>
+                            </Card.Header>
+                            <Card.Subtitle class="section-information">
+                                <p>&emsp;If you are new in town, want to learn something new or just don't know what to do with your free time, check others events.</p>
+                                <p>&emsp;You don't have to take the initiative, just learn new skills from someone else, maybe you will discover your life passion!</p>
+                                <p>&emsp;After join event, we will send you an emails to the event creator. This way you will be able to discuss more details.</p>
+
+
+                            </Card.Subtitle>
+                            <Card.Header class="homePage-section-header">
+                                <h2>Have fun!</h2>
+                                <hr width="40%"/>
+                            </Card.Header>
+
+                        </Card.Body>
+                        <Card.Footer className="text-mute" class="about-footer">-</Card.Footer>
+                    </Card>
+                </div>
                 <Footer/>
             </div>
         );
